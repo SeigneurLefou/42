5a70aa3b279e03d831b51e12e568043ebd311071
Loic Chamard <lchamard@2D3.42angouleme.fr>
2026-02-02 11:43:39 +0100

    feat (dup2): integrate dup 2 and fork

--- pipex/main.c
+++ pipex/main.c
@@ -25,28 +25,52 @@ void	ft_exec_cmd(char *infile, char *cmd_args)
 {
 	char	**cmd;
 	char	*path_cmd;
+	char	*cmd_name;
 
+	(void)infile;
 	cmd = ft_split(cmd_args, ' ');
-	cmd = ft_append(cmd, infile);
-	path_cmd = ft_strjoin("/bin/", cmd[0]);
+	//cmd = ft_append(cmd, infile);
+	cmd_name = cmd[0];
+	path_cmd = ft_strjoin("/bin/", cmd_name);
 	execve(path_cmd, cmd, NULL);
 }
 
 int	main(int argc, char **argv)
 {
 	char	*infile;
-	//int		pipe_fd[2];
+	char	*outfile;
+	int		pipe_fd[2];
 	int 	fdio[2];
+	pid_t		pid;
 
-	if (argc != 5)
+	if (argc != 5 && access(argv[1], R_OK) == -1)
 		return (1);	
 	fdio[0] = open(argv[1], O_RDONLY);
-	fdio[1] = open(argv[4], O_RDONLY);
-	if (access(argv[1], R_OK) == -1)
-		return (1);
 	infile = get_file(fdio[0]);
-	//outfile = get_file(fdio[1])
-	printf("infile : [%s]\n", infile);
-	ft_exec_cmd(infile, argv[2]);
+	pid = fork();
+	if (!pid)
+	{
+		dup2(fdio[0], 0);
+		dup2(1, pipe_fd[1]);
+		exit(0);
+	}
+	fdio[1] = open(argv[4], O_CREAT | O_WRONLY, 0664);
+	pid = fork();
+	if (!pid)
+	{
+		dup2(pipe_fd[0], 0);
+		dup2(1, pipe_fd[1]);
+		exit(0);
+	}
+	pid = fork();
+	if (!pid)
+	{
+		dup2(pipe_fd[0], 0);
+		dup2(1, fdio[1]);
+		exit(0);
+	}
+	outfile = get_file(fdio[1]);
+	printf("infile : [%s], outfile : [%s]\n", infile, outfile);
+	//ft_exec_cmd(infile, argv[2]);
 	return (0);
 }
