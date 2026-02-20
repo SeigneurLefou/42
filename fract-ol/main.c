d73aab4f7620536085408b2d2c7a9fb617e997e7
Loic Chamard <lchamard@1E6.42angouleme.fr>
2026-02-20 16:25:39 +0100

    chore : initiate minilibx

--- fract-ol/main.c
+++ fract-ol/main.c
@@ -6,7 +6,7 @@
 /*   By: lchamard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/19 08:34:48 by lchamard          #+#    #+#             */
-/*   Updated: 2026/02/19 08:41:59 by lchamard         ###   ########.fr       */
+/*   Updated: 2026/02/20 15:45:21 by lchamard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,10 @@
 
 int main(void)
 {
-    void	*mlx;
-    void	*win;
-    
-    // Step 1: Initialize connection to X server
-    mlx = mlx_init();
-    
-    // Step 2: Create a window
-    win = mlx_new_window(mlx, 640, 480, "My Window");
-    
-    // Step 3: Draw something
-    mlx_pixel_put(mlx, win, 320, 240, 0x00FF0000);
-    
-    // Step 4: Register event handlers
-    mlx_key_hook(win, handle_key, NULL);
-    
-    // Step 5: Start event loop (blocks forever)
-    mlx_loop(mlx);
-    
-    return (0);
-	}
+	void	*win;
+
+	win = mlx_init();
+	mlx_new_window(win, 600, 600, "Title");
+	mlx_loop(win);
+	return (0);
 }
